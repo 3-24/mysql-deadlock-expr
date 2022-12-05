b53674dcba850e8306956eb208f28a62686a5726
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2022-12-05 14:55:12 +0900

    Add two modes two expr - deadlock update, and normal update

--- expr.py
+++ expr.py
@@ -16,12 +16,27 @@ conn = mysql.connector.connect(
 
 
 c = conn.cursor()
+"""
 c.execute("SELECT j FROM t WHERE i=%s FOR SHARE;", (i,))
 c.fetchall()
-time.sleep(0.5)
+time.sleep(5)
+start = time.time()
+try:
+    c.execute("UPDATE t SET j=1 WHERE i=%s;", ((i+1) % n,))
+except mysql.connector.errors.InternalError:
+    end = time.time()
+    spent = end - start
+    #print(spent)
+    with open("result.txt", "a") as f:
+        f.write(f"{spent}\n")
+"""
+
 start = time.time()
 c.execute("UPDATE t SET j=1 WHERE i=%s;", ((i+1) % n,))
 end = time.time()
-print(end - start)
+spent = end - start
+with open("result.txt", "a") as f:
+  f.write(f"{spent}\n")
+
 
 conn.close()
